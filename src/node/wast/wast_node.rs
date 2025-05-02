da9355f348965688b46b34ec487dc3720a767355
Hedgehogo <lologlek8@gmail.com>
2025-05-02 16:22:05 +0300

    refactor(node)!: add `Whitespace` as a node

--- src/node/wast/wast_node.rs
+++ src/node/wast/wast_node.rs
@@ -1,21 +1,24 @@
 use super::super::{span::IntoSpanned, whitespace::Side, Expr, Node, Spanned};
-use super::{String, Wast};
+use super::{String, Wast, Whitespace};
 
 #[derive(Debug, Clone, PartialEq, Eq)]
-pub struct WastNode<'input>(pub Wast<'input, Self>);
+pub enum WastNode<'input> {
+    Wast(Wast<'input, Self>),
+    Whitespace(Whitespace<'input>),
+}
 
 impl<'input> Node<'input> for WastNode<'input> {
     type Expr = Vec<Spanned<Self>>;
     type String = String<'input>;
 
     fn from_wast(wast: Wast<'input, Self>) -> Self {
-        Self(wast)
+        Self::Wast(wast)
     }
 }
 
 impl<'input> Expr<'input> for Vec<Spanned<WastNode<'input>>> {
     type Node = WastNode<'input>;
-    type Whitespace = ();
+    type Whitespace = Whitespace<'input>;
 
     fn from_seq(seq: Vec<Spanned<Self::Node>>) -> Self {
         seq
@@ -28,11 +31,23 @@ impl<'input> Expr<'input> for Vec<Spanned<WastNode<'input>>> {
         Some(left.into_spanned(left_span.range.start..right_span.range.end))
     }
 
-    fn whitespaced(
-        expr: Spanned<Self>,
-        _whitespace: Self::Whitespace,
-        _side: Side,
-    ) -> Spanned<Self> {
-        expr
+    fn whitespaced(expr: Spanned<Self>, whitespace: Self::Whitespace, side: Side) -> Spanned<Self> {
+        let Spanned(mut expr, mut span) = expr;
+        match side {
+            Side::Left => {
+                let start = span.range.start - whitespace.repr().len();
+                let node = WastNode::Whitespace(whitespace).into_spanned(start..span.range.start);
+                span.range.start = start;
+                expr.insert(0, node);
+                Spanned(expr, span)
+            }
+            Side::Right => {
+                let end = span.range.end + whitespace.repr().len();
+                let node = WastNode::Whitespace(whitespace).into_spanned(span.range.end..end);
+                span.range.end = end;
+                expr.push(node);
+                Spanned(expr, span)
+            }
+        }
     }
 }
