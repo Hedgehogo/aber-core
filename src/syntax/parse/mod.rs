9d7ec46267844aa7bfbf8deb05fe21b7be92057b
Hedgehogo <lologlek8@gmail.com>
2025-05-15 11:49:23 +0300

    feat(parse)!: add `parser`

--- src/syntax/parse/mod.rs
+++ src/syntax/parse/mod.rs
@@ -11,10 +11,15 @@ pub mod number;
 pub mod raw_string;
 pub mod whitespace;
 
+use super::error::Error;
+use crate::node::{expr::Expr, wast::block::Block};
 use chumsky::{
     combinator::MapWith, error, extra::ParserExtra, input::MapExtra, prelude::*,
     text::unicode::Graphemes,
 };
+use content::content;
+use expr::expr;
+use fact::fact;
 
 pub trait GraphemeParser<'input, O, E>:
     Parser<'input, &'input Graphemes, O, extra::Err<E>>
@@ -40,4 +45,30 @@ where
     E: ParserExtra<'src, I>,
 {
     parser.map_with(|i, e| (i, e.span()))
+}
+
+pub fn parser<'input, X>() -> impl GraphemeParser<'input, Block<'input, X>, Error<'input>> + Clone
+where
+    X: Expr<'input> + 'input,
+{
+    content(expr(fact::<X::Node>()))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    use crate::node::{span::IntoSpanned, CompExpr};
+    use text::Graphemes;
+
+    #[test]
+    fn test_parser() {
+        assert_eq!(
+            parser::<CompExpr>().parse(Graphemes::new("")).into_result(),
+            Ok(Block::new(
+                vec![],
+                CompExpr::from_vec(vec![]).into_spanned(0..0)
+            )),
+        );
+    }
 }
