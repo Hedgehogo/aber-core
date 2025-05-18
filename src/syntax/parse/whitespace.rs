a8a813274b50205a191fa18e5ed4bd446687d523
Hedgehogo <lologlek8@gmail.com>
2025-05-18 16:56:32 +0300

    refactor(parse)!: `line_separator` now returns a line break

--- src/syntax/parse/whitespace.rs
+++ src/syntax/parse/whitespace.rs
@@ -3,7 +3,16 @@ use super::{GraphemeParser, GraphemeParserExtra};
 use crate::node::whitespace::Whitespace;
 use chumsky::prelude::*;
 use smallvec::smallvec;
-use text::{inline_whitespace, newline, Graphemes};
+use text::{inline_whitespace, newline, Grapheme, Graphemes};
+
+pub fn line_break<'input, E, C>() -> impl GraphemeParser<'input, &'input Grapheme, E> + Copy
+where
+    E: GraphemeParserExtra<'input, Error = Error<'input>, Context = Ctx<C>>,
+{
+    newline()
+        .to_slice()
+        .map(|i: &Graphemes| i.iter().next().unwrap())
+}
 
 pub fn line_start<'input, E, C>() -> impl GraphemeParser<'input, (), E> + Copy
 where
@@ -12,11 +21,11 @@ where
     empty()
 }
 
-pub fn line_separator<'input, E, C>() -> impl GraphemeParser<'input, (), E> + Copy
+pub fn line_separator<'input, E, C>() -> impl GraphemeParser<'input, &'input Grapheme, E> + Copy
 where
     E: GraphemeParserExtra<'input, Error = Error<'input>, Context = Ctx<C>>,
 {
-    newline().then(line_start()).ignored()
+    line_break().then_ignore(line_start())
 }
 
 pub fn whitespace<'input, W, E, C>(at_least: usize) -> impl GraphemeParser<'input, W, E> + Copy
@@ -31,8 +40,7 @@ where
 
     let line = inline_whitespace().then(comment.or_not());
 
-    line
-        .then(line_separator().ignore_then(line).repeated())
+    line.then(line_separator().ignore_then(line).repeated())
         .to_slice()
         .map(Graphemes::as_str)
         .try_map(move |i, span| {
@@ -50,7 +58,6 @@ mod tests {
 
     use super::super::tests::Extra;
     use crate::node::wast::Whitespace;
-    use text::Graphemes;
 
     #[test]
     fn test_whitespace() {
